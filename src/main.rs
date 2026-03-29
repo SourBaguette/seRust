5293714cd406d1bd87548008ac5b0b77dda59830
Bishesh Shrestha <bisheshshrestha7@gmail.com>
2026-03-29 23:05:49 +0200

    feat: implement lexer

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,48 @@
+use std::collections::HashMap;
 use std::fs::{self, File};
 use std::io;
 use std::path::Path;
 use xml::reader::{EventReader, XmlEvent};
+
+#[derive(Debug)]
+struct Lexer<'a> {
+    // pointer in memeory where data starts
+    content: &'a [char],
+}
+
+impl<'a> Lexer<'a> {
+    fn new(content: &'a [char]) -> Self {
+        Self { content }
+    }
+
+    fn trim_left(&mut self) {
+        while self.content.len() > 0 && self.content[0].is_whitespace() {
+            // Remove the first character
+            self.content = &self.content[1..];
+        }
+    }
+
+    fn next_token(&mut self) -> Option<&'a [char]> {
+        self.trim_left();
+        // trim white spaces from the left
+        if self.content.len() == 0 {
+            return None;
+        }
+        todo!()
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = &'a [char];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        return self.next_token();
+    }
+}
+
+fn index_document(_doc_content: &str) -> HashMap<String, usize> {
+    todo!("not implemented");
+}
 
 fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> io::Result<String> {
     let file = File::open(file_path)?;
@@ -16,13 +57,21 @@ fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> io::Result<String> {
 }
 
 fn main() -> io::Result<()> {
-    let dir_path = "docs.gl/gl4";
-    let dir = fs::read_dir(dir_path)?;
-    for file in dir {
-        let file_path = file?.path();
-        let content = read_entire_xml_file(&file_path)?;
-        println!("{file_path:?} => {size}", size = content.len());
+    let content = read_entire_xml_file("docs.gl/gl4/glVertexAttribDivisor.xhtml")?
+        .chars()
+        .collect::<Vec<_>>();
+
+    for token in Lexer::new(&content) {
+        println!("{token:?}");
     }
-    // println!("{content}", content = read_entire_xml_file(file_path));
+    // let all_documents = HashMap::<Path, HashMap<String, usize>>::new();
+
+    // let dir_path = "docs.gl/gl4";
+    // let dir = fs::read_dir(dir_path)?;
+    // for file in dir {
+    //     let file_path = file?.path();
+    //     let content = read_entire_xml_file(&file_path)?;
+    //     println!("{file_path:?} => {size}", size = content.len());
+    // }
     Ok(())
 }
