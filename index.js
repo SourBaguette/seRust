506ad36c4f964f58a1abc2404c067a0bef478ead
Bishesh Shrestha <bisheshshrestha7@gmail.com>
2026-04-05 17:14:31 +0200

    feat: divide into model server

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
-// async function search(prompt) {
-//   const results = document.getElementById("results");
-//   results.innerHTML = "";
-//   const response = await fetch("/api/search", {
-//     method: "POST",
-//     headers: { "Content-Type": "text/plain" },
-//     body: prompt,
-//   });
-//   const json = await response.json();
-//   results.innerHTML = "";
-//   for ([path, rank] of json) {
-//     let item = document.createElement("span");
-//     item.appendChild(document.createTextNode(path));
-//     item.appendChild(document.createElement("br"));
-//     results.appendChild(item);
-//   }
-// }
+async function search(prompt) {
+  const results = document.getElementById("results");
+  results.innerHTML = "";
+  const response = await fetch("/api/search", {
+    method: "POST",
+    headers: { "Content-Type": "text/plain" },
+    body: prompt,
+  });
+  const json = await response.json();
+  results.innerHTML = "";
+  for ([path, rank] of json) {
+    let item = document.createElement("span");
+    item.appendChild(document.createTextNode(path));
+    item.appendChild(document.createElement("br"));
+    results.appendChild(item);
+  }
+}
 
-// let query = document.getElementById("query");
-// let currentSearch = Promise.resolve();
+let query = document.getElementById("query");
+let currentSearch = Promise.resolve();
 
-// query.addEventListener("keypress", (e) => {
-//   if (e.key == "Enter") {
-//     currentSearch.then(() => search(query.value));
-//   }
-// });
-console.log("Querying /api/search");
-fetch("/api/search", {
-  method: "POST",
-  headers: { "Content-Type": "text/plain" },
-  body: "bind texture, to buffer",
-}).then((response) => console.log(response));
+query.addEventListener("keypress", (e) => {
+  if (e.key == "Enter") {
+    currentSearch.then(() => search(query.value));
+  }
+});
